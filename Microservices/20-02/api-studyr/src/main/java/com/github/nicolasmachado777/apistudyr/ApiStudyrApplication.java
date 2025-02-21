fa45d6724ce4412f0c13c763327ca67c883568c8
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-20 21:43:41 -0300

    aula2002

---
+++ Microservices/20-02/api-studyr/src/main/java/com/github/nicolasmachado777/apistudyr/ApiStudyrApplication.java
@@ -0,0 +1,13 @@
+package com.github.nicolasmachado777.apistudyr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStudyrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiStudyrApplication.class, args);
+	}
+
+}
---
+++ Microservices/20-02/api-studyr/src/test/java/com/github/nicolasmachado777/apistudyr/ApiStudyrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nicolasmachado777.apistudyr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStudyrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
